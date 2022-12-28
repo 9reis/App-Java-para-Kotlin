c04dda8f8261adffa712b4b10c8db2cb103f7a1c
Lucas Reis <lucasreis_9@hotmail.com>
2022-12-27 22:04:05 -0300

    Iniciada a portacao para Kotlin // Portada a classe Service

--- rest-with-spring-boot-and-java-erudio-destination/src/main/kotlin/com/example/Startup.kt
+++ rest-with-spring-boot-and-java-erudio-destination/src/main/kotlin/com/example/Startup.kt
@@ -1,4 +1,4 @@
-package com.example.restwithspringbootandjavaerudiodestination
+package com.example
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
